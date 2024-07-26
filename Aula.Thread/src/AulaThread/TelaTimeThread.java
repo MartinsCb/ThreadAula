59c3a187c8f8ee0a20504c3cdeca3c84cf4a5efe
marti <andremartins.a@outlook.com>
2024-07-26 15:44:16 -0300

    botões start e stop alinhamento,bloqueio de digitação layout ajuste de layout

--- Aula.Thread/src/AulaThread/TelaTimeThread.java
+++ Aula.Thread/src/AulaThread/TelaTimeThread.java
@@ -4,7 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,16 +18,17 @@ public class TelaTimeThread extends JDialog {
 	
 //new GridBagLayout painel de componentes
 	
-	private JLabel descricaoHora = new JLabel("Time Thread ");
+	private JLabel descricaoHora = new JLabel(" Time Thread ");
 	private JTextField showTime = new JTextField();
 	
 	private JLabel descricaoHora2 = new JLabel("Time Thread 2 ");
 	private JTextField showTime2 = new JTextField();
-	
+	private JButton jButton = new JButton("Start");
+	private JButton jButton2 = new JButton("Stop");
 	public TelaTimeThread() {
 		
 		setTitle("My first screen time with THread");
-		setSize(new Dimension(250, 250) );
+		setSize(new Dimension(300, 300) );
 		setLocationRelativeTo(null);//centraliza a tela 
 		setResizable(false);// não permite alterar dimensões da tela 
 		//primeira parte concluida 
@@ -36,11 +39,16 @@ public class TelaTimeThread extends JDialog {
 		
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth = 2;
+		gridBagConstraints.insets = new Insets(5, 10, 5 , 5);
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		
 		
 		descricaoHora.setPreferredSize(new Dimension(250,20));
 		jPanel.add(descricaoHora, gridBagConstraints);
 		
 		showTime.setPreferredSize(new Dimension(250,20));
+		showTime.setEditable(false);
 		gridBagConstraints.gridy ++; // anda uma coluna para um não ficar sobreposto o outro 
 		jPanel.add(showTime, gridBagConstraints);
 		
@@ -49,8 +57,22 @@ public class TelaTimeThread extends JDialog {
 		jPanel.add(descricaoHora2, gridBagConstraints);
 		
 		showTime2.setPreferredSize(new Dimension(250, 20));
+		showTime2.setEditable(false);
 		gridBagConstraints.gridy ++;
 		jPanel.add(showTime2,gridBagConstraints);
+		
+		gridBagConstraints.gridwidth = 1;
+		
+		jButton.setPreferredSize(new Dimension (92,25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(jButton,gridBagConstraints);
+		
+		jButton2.setPreferredSize(new Dimension (92,25));
+		gridBagConstraints.gridx ++;
+		jPanel.add(jButton2,gridBagConstraints);
+		
+		
+		
 		
 		add(jPanel,BorderLayout.WEST);
 		setVisible(true);
